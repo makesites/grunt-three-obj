e2a33f77d82eb0bc2ead42b86c1827012203318f
Makis Tracend <makis.tracend@gmail.com>
2013-05-14 04:00:47 -0700

    trying a more elegant way to load plugin tasks

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+ * grunt-three-obj
+ * Source: http://github.com/makesites/grunt-three-obj
+ *
+ * Created by [Makis Tracend]( [@tracend](http://github.com/tracend) )
+ *
+ * Released under the [MIT license](http://makesites.org/licenses/MIT)
+ */
+
+module.exports = function(grunt) {
+
+	// Load this plugin's tasks
+	grunt.loadTasks( __dirname +'/tasks');
+
+}
